553efeca70a7fc44e7c2e99e8dc33a719e48b3ec
Brian L. <brian.livramento@hotmail.com>
2026-09-22 17:48:48 +0000

    feat: add payment domain model

---
+++ src/domain/payment/entities/payment.ts
@@ -0,0 +1,50 @@
+import { PaymentMethod } from '../enums/payment-method.enum';
+import { PaymentStatus } from '../enums/payment-status.enum';
+
+export interface PaymentProps {
+  id: string;
+  cpf: string;
+  description: string;
+  amountInCents: number;
+  paymentMethod: PaymentMethod;
+  status: PaymentStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class Payment {
+  private props: PaymentProps;
+
+  constructor(props: PaymentProps) {
+    this.props = props;
+  }
+
+  get id(): string {
+    return this.props.id;
+  }
+
+  get cpf(): string {
+    return this.props.cpf;
+  }
+
+  get description(): string {
+    return this.props.description;
+  }
+
+  get amountInCents(): number {
+    return this.props.amountInCents;
+  }
+
+  get paymentMethod(): PaymentMethod {
+    return this.props.paymentMethod;
+  }
+
+  get status(): PaymentStatus {
+    return this.props.status;
+  }
+
+  updateStatus(status: PaymentStatus): void {
+    this.props.status = status;
+    this.props.updatedAt = new Date();
+  }
+}
